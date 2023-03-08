1e95c61d55db5adcbd8b9d55e2a0c3c3397f3da2
Daniil <borisovdaniil653@gmail.com>
2023-03-08 13:04:42 +0300

    Wrote a programm for C# and added new file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат(число, умноженное само на себя)
+
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(  Console.ReadLine()  );
+int square = num*num;
+Console.WriteLine("Квадрат введенного числа равен "+ square);
+
+if (num<0)
+{
+    Console.WriteLine("Это отрицательное число");
+}
+else
+{
+    Console.WriteLine("Это неотрицательное число");
+}
