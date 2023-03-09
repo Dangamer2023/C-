8091afb04698f15a862e6f0f7dc1e73274fd4228
Daniil <borisovdaniil653@gmail.com>
2023-03-09 14:18:16 +0300

    Done the second task

--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
-﻿//Напишите программу, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
-//a = 2, b =10 > нет
-//a = 9, b = 3 > да
-// и т.д
+﻿//Напишите программу, которая выдает день недели по заданному номеру
 
-Console.WriteLine("Введите первое число");
-int first = Convert.ToInt32(  Console.ReadLine()  );
-Console.WriteLine("Введите второе число");
-int second = Convert.ToInt32(  Console.ReadLine()  );
+Console.WriteLine("Введите число от 1 до 7");
+string DayNum = Console.ReadLine();
 
-int square = second*second;
-
-if (square == first)
-{
-    Console.WriteLine("Является");
-}
-else
-{
-     Console.WriteLine("Не является");
-}
+if(DayNum == "1") Console.WriteLine("Понедельник");
+else if(DayNum == "2") Console.WriteLine("Вторник");
+else if(DayNum == "3") Console.WriteLine("Среда");
+else if(DayNum == "4") Console.WriteLine("Четверг");
+else if(DayNum == "5") Console.WriteLine("Пятница");
+else if(DayNum == "6") Console.WriteLine("Суббота");
+else if(DayNum == "7") Console.WriteLine("Воскресенье");
+else Console.WriteLine("Введите число от 1 до 7 включительно");
