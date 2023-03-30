c85ab78d016ecec9f08dc54082613aa5b8e50154
Daniil <borisovdaniil653@gmail.com>
2023-03-30 15:10:38 +0300

    Solved more tasks

--- Program.cs
+++ Program.cs
@@ -8,11 +8,10 @@
 
 23432 -> да*/
 
-void Pallidrom(int n)
+void Pallindrom(int number)
 {
-Console.WriteLine("Введите пятизначное число");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Ваше число: {number}");
+
+
 int d1 = number / 10000;
     int d2 = (number / 1000) % 10;
     int d3 = (number / 100) % 10;
@@ -29,6 +28,13 @@ else
 
 
 
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ваше число: {number}");
+Pallindrom(number);
+
+
+
 /*Задача 21
 
 /*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
@@ -37,31 +43,30 @@ A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
-/*void Coordinates(int XYZ)
+void Coordinates()
 {
+double aX, aY, aZ, bX, bY, bZ;
+
 Console.WriteLine("Введите координаты точки A (x,y,z):");
-    string[] aStr = Console.ReadLine().Split(',');
-    double aX = double.Parse(aStr[0]);
-    double aY = double.Parse(aStr[1]);
-    double aZ = double.Parse(aStr[2]);
+string[] aStr = Console.ReadLine().Split(',');
+aX = double.Parse(aStr[0]);
+aY = double.Parse(aStr[1]);
+aZ = double.Parse(aStr[2]);
 
-    Console.WriteLine("Введите координаты точки B (x,y,z):");
-    string[] bStr = Console.ReadLine().Split(',');
-    double bX = double.Parse(bStr[0]);
-    double bY = double.Parse(bStr[1]);
-    double bZ = double.Parse(bStr[2]);
+Console.WriteLine("Введите координаты точки B (x,y,z):");
+string[] bStr = Console.ReadLine().Split(',');
+bX = double.Parse(bStr[0]);
+bY = double.Parse(bStr[1]);
+bZ = double.Parse(bStr[2]);
 
-    double dist = Math.Sqrt(Math.Pow(aX - bX, 2) + Math.Pow(aY - bY, 2) + Math.Pow(aZ - bZ, 2));
+double dist = Math.Sqrt(Math.Pow(aX - bX, 2) + Math.Pow(aY - bY, 2) + Math.Pow(aZ - bZ, 2));
 
-    Console.WriteLine("Расстояние между точками: {0:F2}", dist);
-}*/
-
-
+Console.WriteLine("Расстояние между точками: {0:F2}", dist);
+}
 
 
 
-
-
+Coordinates();
 
 
 
@@ -74,32 +79,23 @@ Console.WriteLine("Введите координаты точки A (x,y,z):");
 5 -> 1, 8, 27, 64, 125*/
 
 
-/*void Main()
+ void Main()
     {
         Console.Write("Введите число N: ");
         int n = int.Parse(Console.ReadLine());
 
         Console.WriteLine("Таблица кубов чисел от 1 до {0}:", n);
-        PrintCubeTable(n);
-    }
-
-
-
-void PrintCubeTable(int n)
-    {
+   
         for (int i = 1; i <= n; i++)
         {
             int cube = i * i * i;
             Console.Write("{0} ", cube);
         }
         Console.WriteLine();
-    }*/
+    }
 
 
-
-
-
-
+Main();
 
 
 
